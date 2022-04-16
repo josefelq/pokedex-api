eb1f629e4cc5eed0c7f7870ef52ddabf4e6daa2c
Jose Felipe <josefelipeq@live.com>
2022-04-15 22:42:16 -0500

    refactor: remove unused imports

--- pokedex/pokedex/urls.py
+++ pokedex/pokedex/urls.py
@@ -3,32 +3,16 @@ from django.views.generic import TemplateView
 
 from django.urls import path, include
 
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
+
 from drf_spectacular.views import (
     SpectacularAPIView,
-    SpectacularRedocView,
     SpectacularSwaggerView,
 )
 
-
-# schema_view = get_schema_view(
-#    openapi.Info(
-#       title="Snippets API",
-#       default_version='v1',
-#       description="Test description",
-#       terms_of_service="https://www.google.com/policies/terms/",
-#       contact=openapi.Contact(email="contact@snippets.local"),
-#       license=openapi.License(name="BSD License"),
-#    ),
-#    public=True,
-#    permission_classes=[],
-# )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
